8c50e8b24a9285c24c16185b9e1e74340447fbbb
Dmitry Pushkin <d.pushkin@limecreditgroup.com>
2025-02-26 11:43:08 +0300

    добавил получение последних трат

--- src/expense/handlers/statistics.py
+++ src/expense/handlers/statistics.py
@@ -6,16 +6,17 @@ from telegram import (
 from telegram.ext import ContextTypes
 
 from src.database import db_helper
-from src.expense.service import get_statistics_by_months_count
+from src.expense.service import get_statistics_by_months_count, get_top_expense
 from src.handlers import main_keyboard
 
-MONTH_STAT, THREE_MONTHS_STAT = range(2)
+MONTH_STAT, THREE_MONTHS_STAT, TOP_EXPENSE = range(3)
 
 session = db_helper.session_factory()
 
 
 async def get_statistic_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     buttons = [
+        [InlineKeyboardButton(text="Последние 10 трат", callback_data=str(TOP_EXPENSE))],
         [InlineKeyboardButton(text="Статистика за текущий месяц", callback_data=str(MONTH_STAT))],
         [InlineKeyboardButton(text="Статистика за три месяца", callback_data=str(THREE_MONTHS_STAT))],
     ]
@@ -34,6 +35,17 @@ async def get_month_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
         resume += stat.amount
         answer += f"{stat.category_name.capitalize()}: {stat.amount}\n"
     answer += f"\nИтого: {resume}"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=answer, reply_markup=main_keyboard)
+
+
+async def get_top_expense_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    user_telegram_id = update.effective_user.id
+    statistics = await get_top_expense(session, user_telegram_id)
+    answer = "Последние 10 трат\n\n"
+    for stat in statistics:
+        answer += f"{stat.category_name.capitalize()}: {stat.amount}\n"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer, reply_markup=main_keyboard)
 
 
